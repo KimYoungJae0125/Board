eec11c342deac8da00d84683eeebc65644245316
KimYoungJae <uni0125@naver.com>
2022-08-06 12:17:03 +0900

    Modfiy https(Js)
    Get 방식 추가

--- src/main/resources/static/js/common/https.js
+++ src/main/resources/static/js/common/https.js
@@ -1,5 +1,8 @@
+//type과 interface 의 용도는 비슷하다.
 const Https = {
-    Get: () => {
+    Get: (requestUrl) => {
+        const option = { method: "GET" };
+        return fetch(requestUrl, option).then(response => response.json());
     },
     Post: function (requestUrl) {
         const option = { method: "POST" };
@@ -8,7 +11,7 @@ const Https = {
             option["body"] = JSON.stringify(requestData);
             return fetch(requestUrl, option).then(response => response.json());
         };
-        return this; //화살표 함수의 this는 window 객체를 가리킨다. 현재 객체를 this 키워드로 반환하고 싶으면 화살표 함수가 아닌 일반 함수를 사용한다.
+        return this; //화살표 함수의 this는 window(현재 변수 상위 스코프) 객체를 가리킨다. 현재 객체를 this 키워드로 반환하고 싶으면 화살표 함수가 아닌 일반 함수를 사용한다.
     },
     Put: () => {
     },
--- src/main/resources/static/ts/common/https.ts
+++ src/main/resources/static/ts/common/https.ts
@@ -1,3 +1,5 @@
+//type과 interface 의 용도는 비슷하다.
+
 type httpMethod = {
         Get :Function;
         Post :Function;
@@ -5,27 +7,39 @@ type httpMethod = {
         Patch :Function;
         Delete :Function;
     }
-const Https :httpMethod = {
-      Get : () :void => {
 
+interface response {
+    transactionTime :Date;
+    statusCode :number;
+    responseMessage :string;
+    description :string;
+    data :any;
+    errors :Array<any>;
+}
+
+const Https :httpMethod = {
+      Get : (requestUrl :string) :Promise<response> => {
+          const option = {method : "GET"}
+
+          return fetch(requestUrl, option).then(response => response.json());
       }
     , Post : function(requestUrl :string) :Function {
         const option = {method : "POST"}
 
-        this.Json = (requestData :object) :Promise<any> => {
+        this.Json = (requestData :object) :Promise<response> => {
             option["headers"] = {"Content-Type" : "application/json"}
             option["body"] = JSON.stringify(requestData);
 
             return fetch(requestUrl, option).then(response => response.json());
         }
 
-        return this;    //화살표 함수의 this는 window 객체를 가리킨다. 현재 객체를 this 키워드로 반환하고 싶으면 화살표 함수가 아닌 일반 함수를 사용한다.
+        return this;    //화살표 함수의 this는 window(현재 변수 상위 스코프) 객체를 가리킨다. 현재 객체를 this 키워드로 반환하고 싶으면 화살표 함수가 아닌 일반 함수를 사용한다.
       }
     , Put : () :void => {
       }
     , Patch : () :void => {
       }
-    , Delete : (requestUrl :string) :Promise<any> => {
+    , Delete : (requestUrl :string) :Promise<response> => {
         const option = {method : "DELETE"}
 
         return fetch(requestUrl, option).then(response => response.json());
